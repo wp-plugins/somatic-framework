d43bf0b061cdb7da7561eb5c0df2a8edeb006ade
somatic <somatic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-23 22:53:41 +0000

    pushing v1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/somatic-framework/tags/1.3.2@509548 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ doc/meta-config-example.php
@@ -0,0 +1,243 @@
+<?php
+//** This code exists solely to declare the data for custom metaboxes with the somaticFramework! **//
+//** it won't do you much good if you don't have the Somatic Framework installed **//
+
+
+add_action('admin_init', 'my_meta_box_data' );								// define metabox vars only in admin
+
+// add_action('soma_field_type_case', 'media_field_type', 10, 4);
+// add_filter('soma_field_fetch_meta', 'fetch_media_field', 10, 3);
+
+
+//** you should only set these options when activating your plugin/theme **//
+
+// update_option('soma_meta_serialize', 1);			// If you want to store all post_meta items as a serialized array in a single key, you need to set the option 'soma_meta_serialize' to 1 (true) (false (0) by default)
+// update_option('soma_meta_prefix', '_soma');			// this is the default prefix added to all post_meta keys. Change if desired.
+
+//**  END options  **//
+
+
+
+// outline and inject metaboxes and fields into the somaMetaboxes data container
+function meta_box_data() {
+	if ( !class_exists("somaticFramework") ) return null; // only proceed if framework plugin is active
+
+	// call the framework function for each individual metabox to be rendered
+	soma_metabox_data( array(
+		'id' => 'track-meta-box',
+		'title' => 'Track Info',											// text displayed in title of metabox
+		'types' => array('tracks'),											// which post types this metabox is output for
+		'context' => 'normal',												// positioning: normal/side columns
+		'priority' => 'high',												// positioning: vertical order - if all have same priority, metaboxes are rendered in order they appear in somaMetaboxes::$data array
+		'restrict' => false,												// boolean for restricting display of this metabox for non-staff (a special somaticFramework permission class)
+		'save' => true,														// boolean for displaying a "save changes" button at the end of this metabox (can have multiple on the page)
+		'fields' => array(													// array of individual fields within this metabox
+			array(
+				'name' => 'Actor Name',										// text displayed alongside field input
+				'id' => 'actor_name', 										// used when saving, should be the name of the post_meta (key) or taxonomy (exact slug) we're manipulating
+				'type' => 'text',											// field type (usually input: text, area, select, checkbox, radio), sometimes output (posts, other readonly data)
+				'data' => 'meta',											// what kind of data is being retrieved and saved for this post (meta [wp_postmeta table], core [wp_posts table], taxonomy, user, p2p, attachment, comment)
+				'options' => soma_select_taxonomy_terms('artists'),			// array of options to populate html form input objects (in this case, generated automatically from available taxonomy terms)
+				'default' => '',											// default value to show (in text fields or selectors)
+				'multiple' => false,										// can multiple values be selected? or must the saved value be singular?
+				'required' => true											// can this field be left empty or unselected? enables red styling to draw attention (validation functions to check completion don't exist yet)
+			),
+		)
+	));
+	
+	soma_metabox_data( array(
+		'id' => 'resource-meta-box',
+		'title' => 'Metadata',
+		'types' => array('resource'),
+		'context' => 'normal',
+		'priority' => 'high',
+		'restrict' => false,
+		'save' => true,
+		'fields' => array(
+			array(
+				'name' => 'Content Type',									// Label for this field
+				'id' => 'content',											// id matching the taxonomy slug we're manipulating
+				'type' => 'select',											// renders dropdown input selector
+				'options' => soma_select_taxonomy_terms('content'),
+				'data' => 'taxonomy',
+				'multiple' => false,
+			),
+			array(
+				'name' => 'Author(s)',
+				'id' => 'author-list',
+				'type' => 'p2p-list',
+				'data' => 'p2p',
+				'p2pname' => 'authors-works',
+				'dir' => 'to',
+			),
+			array(
+				'name' => 'Publication Info',
+				'id' => 'publication',
+				'type' => 'text',
+				'data' => 'meta',
+				'desc' => 'When was this published and by whom?',
+			),
+			array(
+				'name' => 'SKU#',
+				'id' => 'sku',
+				'type' => 'numeric',
+				'data' => 'meta',
+				'desc' => 'Is this item sold in the Mises Store? Enter SKU for a link to be generated automatically',
+			),
+			array(
+				'name' => 'ISBN',
+				'id' => 'isbn',
+				'type' => 'numeric',
+				'data' => 'meta',
+				'desc' => 'If this is a printed book, please include ISBN',
+			),
+			array(
+				'name' => 'Subjects',
+				'id' => 'subject',
+				'type' => 'checkbox-multi',
+				'options' => somaMetaboxes::select_taxonomy_terms('subject'),
+				'data' => 'taxonomy',
+				'multiple' => true,
+			),
+			array(
+				'name' => 'Collection',
+				'id' => 'collection',
+				'type' => 'select',
+				'options' => somaMetaboxes::select_taxonomy_terms('collection'),
+				'data' => 'taxonomy',
+				'multiple' => true,
+			),
+			array(
+				'name' => 'Source',
+				'id' => 'source',
+				'type' => 'select',
+				'options' => somaMetaboxes::select_taxonomy_terms('source'),
+				'data' => 'taxonomy',
+				'multiple' => false,
+				'required' => true
+			),
+			array(
+				'name' => 'Streaming Media URL',
+				'id' => 'streaming-media',
+				'type' => 'text',
+				'data' => 'meta',
+				'desc' => 'Enter link to YouTube, Vimeo, iTunes, etc.',
+			),
+			array(
+				'name' => 'Description',
+				'id' => 'post_content',
+				'type' => 'richtext',
+				'rows' => 30,
+				'data' => 'core',
+				'desc' => 'This is the content that will display on the resource page',
+			),
+			array(
+				'name' => 'Excerpt',
+				'id' => 'post_excerpt',
+				'type' => 'textarea',
+				'data' => 'core',
+				'desc' => 'Excerpts are optional hand-crafted summaries that may be used when space is tight',
+			),
+			array(
+				'name' => 'HTML Content',
+				'id' => 'html',
+				'type' => 'html',
+				'rows' => 30,
+				'data' => 'meta',
+				'desc' => 'If this resource is available in HTML format, paste it here',
+			),
+		)
+	));
+}
+
+
+
+	// HOOKS FOR SOMATIC FRAMEWORK METABOX SYSTEM
+
+	/**
+	* fetch metadata in custom cases
+	* hooks into soma_field_fetch_meta filter
+	*
+    * @access private
+    * @param $meta retrieved post metadata
+    * @param $post current post object
+    * @param $field current metabox field from global $customMetaConfig
+    * @return $meta OBJECT
+	*/
+	function fetch_media_field($meta, $post, $field) {
+		// get attached media object for this post (only returns one)
+		if ($field['data'] == 'media') {
+			$meta = somaFunctions::fetch_attached_media($post->ID, $field['type']);
+		}
+		return $meta;
+	}
+
+	/**
+	* custom metabox field with upload button and media player preview
+	* hooks into soma_metabox_type_case action
+	* must contain a conditional statement to only output the field when the type matches!
+	* echos metabox field content
+	*
+    * @access private
+    * @param $post current post object
+    * @param $meta retrieved post metadata (in this case, the object attached to the post)
+    * @param $field current metabox field from global $customMetaConfig
+    * @param $complete boolean
+	*/
+
+	function media_field_type($post, $meta, $field, $complete) {
+		if ($field['type'] == "audio" || $field['type'] == "video") {
+			$label = $meta ? "Modify Media File" : "Upload Media File";
+			echo "<a href=\"media-upload.php?post_id=$post->ID&amp;TB_iframe=1&amp;height=800&amp;width=640\" id=\"add_media\" class=\"thickbox clicker\" onclick=\"return false;\">$label</a>";
+			echo $meta ? "" : "<p class=\"howto\">{$field['desc']}</p>";
+			// don't attempt to show player if no media exist yet
+			if (!$meta) return null;
+			// cycle through array of attached media objects
+			foreach ($meta as $media) {
+				$url = wp_get_attachment_url( $media->ID);
+				// output jplayer
+				echo "<div id=\"jplayer-admin\" class=\"jp-jplayer\" type=\"{$field['type']}\" rel=\"$url\"></div>";
+?>
+<div class="jp-audio-container">
+	<div class="jp-audio">
+		<div class="jp-type-single">
+			<div id="jp_interface_1" class="jp-interface">
+				<ul class="jp-controls">
+					<li><a href="#" class="jp-play" tabindex="1">play</a></li>
+					<li><a href="#" class="jp-pause" tabindex="1">pause</a></li>
+					<li><a href="#" class="jp-stop" tabindex="1">stop</a></li>
+					<li><a href="#" class="jp-mute" tabindex="1">mute</a></li>
+					<li><a href="#" class="jp-unmute" tabindex="1">unmute</a></li>
+				</ul>
+				<div class="jp-progress-container">
+					<div class="jp-progress">
+						<div class="jp-seek-bar">
+							<div class="jp-play-bar"></div>
+						</div>
+					</div>
+				</div>
+				<div class="jp-volume-bar-container">
+					<div class="jp-volume-bar">
+						<div class="jp-volume-bar-value"></div>
+					</div>
+				</div>
+				<!-- not supported in blackyellow skin -->
+				<!-- <div class="jp-current-time"></div>  -->
+				<!-- <div class="jp-duration"></div>  -->
+			</div>
+			<div id="jp_playlist_1" class="jp-playlist">
+				<ul>
+					<li><?php echo $media->post_title; ?></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+				echo $field['type'] ? "" : "<em>Must specify a format first!</em>";
+				echo "<div id=\"jplayer_inspector\"></div>";
+			}
+
+		}
+	}
+?>
---
+++ inc/somaDownload.php
@@ -0,0 +1,163 @@
+<?php
+// force download of single or group of attachment id's
+class somaDownload extends somaticFramework {
+	
+	protected $_asset_id;
+	
+	function __construct($id) {
+		
+		// if (!is_admin()) return;	// admin only?
+		// $this->$_asset_id = $id;
+		check_admin_referer( 'soma-download', 'security' ); // will die if invalid or missing nonce
+
+		// init group
+		$group = array();
+
+
+		// transfer requested image size
+		if (isset( $_GET['getsize'])) {
+			$size = $_GET['getsize'];
+		} else {
+			$size = "full";
+		}
+
+		// transfer eventual filename of zip file
+		if (isset( $_GET['zipname'] ) ) {
+			$group['zipname'] = sanitize_title($_GET['zipname']) . ' - ' . $_GET['getsize'];
+		} else {
+			$group['zipname'] = "Selected Items - " . $_GET['getsize'];
+		}
+
+
+		// build array of items to be processed for download
+
+		//-- if we're viewing a single batch, then the download group should grab all the connected children of this batch
+		if (isset( $_GET['batch'] ) ) {
+			// init item array
+			$items = array();
+			$children = get_posts( array(
+				'suppress_filters' => false,
+				'post_type' => 'any',
+				'post_status' => 'any',
+				'connected_from' => intval($_GET['batch']),
+				'numberposts' => -1	// needed to return all posts, not just the first 5
+			) );
+			// extract just the queried posts ID
+			foreach ($children as $child) {
+				$items[] = $child->ID;
+			}
+			// remove duplicates
+			$items = array_unique($items);
+			if (!$items || empty($items)) {
+				wp_die('no items matched the download request!');
+			}
+		//-- package request
+		} else if (isset( $_GET['package'] ) ) {
+			// init item array
+			$items = array();
+			// retrieve stills with the requested package taxonomy term
+			// $members = new WP_Query( array(
+			// 	'post_type' => 'stills',
+			// 	// 'post_status' => 'publish',
+			// 	'taxonomy' => 'package',
+			// 	'term' => $_GET['package'],
+			// 	'posts_per_page' => -1
+			// ) );
+			$members = get_posts( array(
+				'suppress_filters' => false,
+				'post_type' => 'stills',
+				// 'post_status' => 'publish',
+				'taxonomy' => 'package',
+				'term' => $_GET['package'],
+				'numberposts' => -1
+			) );
+			// extract just the queried posts ID
+			foreach ($members as $member) {
+				$items[] = $member->ID;
+			}
+			if (isset( $_GET['include_releases'] ) ) {
+			// retrieve assigned releases for each item
+				foreach ($items as $item) {
+					$releases = get_posts( array(
+						'suppress_filters' => false,
+						'post_type' => 'releases',
+						'post_status' => 'publish',
+						'connected_from' => $item,
+						'posts_per_page' => -1
+					) );
+					foreach ($releases as $release) {
+						// append connected release to the package list of items
+						$items[] = $release->ID;
+					}
+				}
+			}
+			// remove duplicates
+			$items = array_unique($items);
+			if (!$items || empty($items)) {
+				wp_die('no items matched the download request!');
+			}
+			// override the zipname with this package slug name
+			$package = get_term_by('slug', $_GET['package'], 'package');
+			$group['zipname'] = $package->slug . '-' . $size;
+
+		//-- generic request, just grab serialized array of posts from the current query passed in _GET
+		} else {
+			if ( isset( $id ) ) {
+				$items = maybe_unserialize( $id );
+				// convert to array if not yet
+				if (!is_array($items)) {
+					$items = array($items);
+				}
+			} else {
+				wp_die('no items to download!');
+			}
+		}
+
+		// init group items
+		$group['items'] = array();
+
+		//
+		foreach ($items as $item) {
+			// populate the group with each item
+			$group['items'][] = array(
+				// retrieve path to original file
+				'source' => get_attached_file($item),
+				// name to be given to downloaded file
+				'title' => get_post_meta($item, '_wp_attached_file', true),
+				// mime-type
+				'mime' => get_post_mime_type($item)
+			);
+		}
+
+		// single file, just download it
+		if (count($group['items']) == 1) {
+			$item = $group['items'][0];
+			$size = filesize($item['source']);
+			header("Cache-Control: no-cache, must-revalidate"); 	// HTTP/1.1
+			header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 		// Date in the past
+			header("Content-Disposition: attachment; filename={$item['title']}");
+			header("Content-type: {$item['mime']}");
+			header("Content-Length: $size");
+			readfile($item['source']);
+		} else {
+		// group of files, create a zip
+			$zipfile = new zipfile();						// use zip_min.inc.php class
+			$zipname = $group['zipname'].'.zip';
+
+			foreach ( $group['items'] as $item) {
+				$zipfile -> addFile(file_get_contents($item['source']), $item['title']);
+			}
+			// Force download the zip
+			header("Cache-Control: no-cache, must-revalidate"); 	// HTTP/1.1
+			header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 		// Date in the past
+			header("Content-type: application/octet-stream");
+			header("Content-Disposition: attachment; filename=$zipname");
+			echo $zipfile -> file();
+
+		}
+
+		// save zip to server instead
+		// $contents = $zipfile -> file();
+		// file_put_contents("test.zip", $contents);
+	}
+}
