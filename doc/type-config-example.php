244be0de29bc742ba91fdda28c7791cfd4e97e9c
somatic <somatic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-24 06:57:39 +0000

    included a bit too much specific code from mises in the example docs
    git-svn-id: https://plugins.svn.wordpress.org/somatic-framework/trunk@509675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- doc/type-config-example.php
+++ doc/type-config-example.php
@@ -4,26 +4,24 @@
 
 
 add_action( 'init', 'init' );
-add_filter( 'parse_query', 'filter_custom_query' );
-add_filter( 'p2p_connectable_args', 'order_authors_by_title', 10, 2 );
 add_action( 'soma_column_data', 'custom_column_data', 10, 2);
 
 function init() {
 	// only proceed if framework plugin is active
 	if ( !class_exists("somaticFramework") ) return null;
 
-
+	$my_image_directory = 
 	//
 	soma_init_type( array(
 		"slug" => "resource",													// primary identifier
 		"single" => "Resource",													// used to build labels
 		"plural" => "Resources",												// used to build labels
-		'args' => array(														// optional argument overrides http://codex.wordpress.org/Function_Reference/register_post_type
+		"args" => array(														// optional argument overrides http://codex.wordpress.org/Function_Reference/register_post_type
 				'menu_position' => 5,											// where the admin menu item where sit
 				'supports' => array( 'title', 'thumbnail'),						// which features and core metaboxes to show
 				'public' => true,
 			),
-		'icons' => MOCMS_IMG,
+		"icons" => "http://mysite.com/mytheme/img/",							// set url path to where your custom icons are located
 		"columns" => array(														// custom columns for edit.php listing
 				"cb" => "<input type=\"checkbox\" />",
 				"thumb" => "Cover",
@@ -46,7 +44,7 @@ function init() {
 				'supports' => array( 'editor', 'title', 'thumbnail' ),
 				'public' => true,
 			),
-		'icons' => MOCMS_IMG,
+		'icons' => "http://mysite.com/mytheme/img/",
 		"columns" => array(
 				"cb" => "<input type=\"checkbox\" />",
 				"thumb" => "Photo",
@@ -55,30 +53,9 @@ function init() {
 			)
 		)
 	);
-	
+
 	//
-	soma_init_type( array(
-		"slug" => "daily",
-		"single" => "Daily Article",
-		"plural" => "Daily Articles",
-		'args' => array(
-				'menu_position' => 7,
-				'supports' => array( 'comments', 'title', 'thumbnail', 'excerpt', 'editor' ),
-				'public' => true,
-			),
-		'icons' => MOCMS_IMG,
-		"columns" => array(
-				"cb" => "<input type=\"checkbox\" />",
-				"thumb" => "Thumb",
-				"title" => "Article Title",
-				"resourceauthor" => "Author",
-				"author" => "Editor",
-				"date" => "Date"
-			)
-		)
-	);
-
-
+	
 	soma_init_taxonomy( array(
 			"slug" => "format",												// primary identifier
 			"single" => "Format",											// used to build labels
@@ -116,146 +93,40 @@ function init() {
 			)
 		)
 	);
-	
-	soma_init_taxonomy( array(
-			"slug" => "subject",
-			"single" => "Subject",
-			"plural" => "Subjects",
-			"types" => array( 'resource', 'daily' ),
-			"args" => array( 'hierarchical' => true ),
-			"metabox" => false,
-			'terms' => array(
-				'Socialism',
-				'Capitalism',
-				'Taxation',
-				'Core',
-				'Biography',
-				'Government',
-				'Monetary Policy'
-			)
-		)
-	);
-	
-	soma_init_taxonomy( array(
-			"slug" => "keyword",
-			"single" => "Keyword",
-			"plural" => "Keywords",
-			"types" => array( 'resource', 'daily' ),
-			"args" => array( 'hierarchical' => false ),
-		)
-	);
-	
-	
-	soma_init_taxonomy( array(
-			"slug" => "collection",
-			"single" => "Collection",
-			"plural" => "Collections",
-			"types" => array( 'resource' ),
-			"args" => array( 'hierarchical' => true ),
-			"metabox" => false,
-			'terms' => array(
-				'Mises University',
-				'Austrian Scholars Conference',
-				'Mises Circle Seminars',
-				'Lecture Series',
-				'Beginner\'s Guide',
-				'Mises Academy'
-			)
-		)
-	);
-	
-	
-	soma_init_taxonomy( array(
-			"slug" => "source",
-			"single" => "Source",
-			"plural" => "Sources",
-			"types" => array( 'resource' ),
-			"args" => array( 'hierarchical' => true ),
-			"metabox" => false,
-			'terms' => array(
-				'Quarterly Journal of Austrian Economics',
-				'The Freeman',
-				'Libertarian Papers',
-				'Journal of Libertarian Studies',
-				'Austrian Economics Newsletter',
-				'American Affairs',
-			)
-		)
-	);
 
-	// establish relationships
-	
-	if ( !function_exists( 'p2p_register_connection_type' ) )
-		return;
 
-	p2p_register_connection_type( array(
-		'id' => 'authors-works',
-		'from' => 'author',
-		'to' => array( 'resource','daily'),
-		'sortable' => 'any',
-		'title' => array( 'to' => 'Authored By', 'from' => 'Works by this Author' )
-	) );
-
-	p2p_register_connection_type( array(
-		'id' => 'daily-resources',
-		'from' => 'daily',
-		'to' => array('resource'),
-		'title' => array( 'to' => 'Used in Daily Articles', 'from' => 'Resource References' ),
-		'can_create_post' => false
-	) );
-
-	
+	// establish relationships	
+	if ( function_exists( 'p2p_register_connection_type' ) ) {
+		p2p_register_connection_type( array(
+			'id' => 'authors-works',
+			'from' => 'author',
+			'to' => array( 'resource','daily'),
+			'sortable' => 'any',
+			'title' => array( 'to' => 'Authored By', 'from' => 'Works by this Author' )
+		) );
+	}
 }
+
 
 function custom_column_data($column, $post) {
 	// output each column's content
 	switch ($column) {
+		// p2p connected column output
 		case "authors":
 			echo somaFunctions::fetch_connected_items($post->ID, 'authors-works');
 		break;
+		// taxonomy column output
 		case "format":
 			echo somaFunctions::fetch_the_term_list( $post->ID, 'format','',', ');
 		break;
-		case "source":
-			echo somaFunctions::fetch_the_term_list( $post->ID, 'source','',', ');
-		break;
-		case "content":
-			echo somaFunctions::fetch_the_term_list( $post->ID, 'content','',', ');
-		break;
+		// post_meta column output
 		case "notes":
 			echo somaFunctions::asset_meta('get', $post->ID, 'notes');
 		break;
-		case "institution":
-			echo somaFunctions::asset_meta('get', $post->ID, 'institution');
-		break;
+		// custom column output
 		case "email":
 			$email = somaFunctions::asset_meta('get', $post->ID, 'email');
 			echo "<a href=\"mailto:$email\">$email</a>";
 		break;
 	}
-}
-
-
-// modifies the QUERY before output
-function filter_custom_query($query) {
-	// sort our custom types and taxonomies by menu_order instead of date, so we can manually adjust their display order with somaSorter
-	if ( $query->query['post_type'] == 'tracks'
-		|| isset( $query->query['genres'] )
-		|| isset( $query->query['album'] )
-		|| isset( $query->query['formats'] )
-		|| isset( $query->query['artists'] )
-		&& !$query->query_vars['suppress_filters']) {
-		$query->set( 'orderby', 'menu_order' );
-		$query->set( 'order', 'ASC' );
-	}
-	return $query;
-}
-
-// modify p2p lists of related authors to sort alphabetically rather than default chronological
-function order_authors_by_title( $args, $ctype ) {
-	if ( 'authors_to_dailys' == $ctype->id ) {
-		$args['orderby'] = 'title';
-		$args['order'] = 'asc';
-	}
-	return $args;
 }
